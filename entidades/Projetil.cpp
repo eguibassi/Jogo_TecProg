49237458b77f4fc400bee935a20974731897b9eb
Arthur <amurillov0711@gmail.com>
2026-05-25 18:01:01 -0300

    projetil.cpp

--- entidades/Projetil.cpp
+++ entidades/Projetil.cpp
@@ -1,11 +1,40 @@
+#include "Projetil.h"
+
 using namespace Entidades;
 
-    Projetil::Projetil() : Entidade(),vx(0), dano(0),ativo(true){}
-    Projetil::~Projetil(){
-        vx = -1;
-        dano = -1;
-        ativo = false
+Projetil::Projetil():
+    Entidade(),
+    vx(0),
+    dano(0),
+    ativo(true){}
+
+Projetil::~Projetil(){}
+
+void Projetil::setVx(int n){vx = n;}
+int Projetil::getVx() const{return vx;}
+
+void Projetil::setDano(int d){dano = d;}
+int Projetil::getDano() const{return dano;}
+
+bool Projetil::getAtivo() const{return ativo;}
+void Projetil::setAtivo(bool a){ativo = a;}
+
+void Projetil::executar()
+{
+    if (!ativo)
+    {
+        return;
     }
 
-    Projetil::setVx(int n){vx = n;}
-    Projetil::getVx()
+    sprite.move(vx, 0);
+
+    if (sprite.getPosition().x < 0 || sprite.getPosition().x > 1280)
+    {
+        ativo = false;
+    }
+}
+
+void Projetil::salvar()
+{
+    //implementar futuramente
+}
